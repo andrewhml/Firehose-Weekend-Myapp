fa2ce894e58d64f9e64f620ad14383ff04df97a5
Andrew Lee <ALee@Andrews-MacBook-Pro-2.local>
2013-09-14 18:01:03 -0400

    Adding form functionality to pics

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,24 @@
+class PicsController < ApplicationController
+	#This method helps us create a new picture
+	def new
+		@pic = Pic.new
+	end
+
+	def index
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+end
